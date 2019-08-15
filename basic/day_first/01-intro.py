5239a3dc566a4411b6b19472e6f78acb4160ac24
Artem Manchenkov <artyom@manchenkoff.me>
2019-08-15 21:13:36 +0300

    day 01 update

--- basic/day_first/01-intro.py
+++ basic/day_first/01-intro.py
@@ -6,3 +6,22 @@
 #  Примеры базового синтаксиса и работа с типами данных
 #  Числа, строки, списки, булево значение
 #
+
+a = 1 + 2
+a = a + a
+
+integer_number = 10  # int
+float_number = 10.4  # float
+
+string_value = "Text"  # str
+string_value = string_value * 2  # = TextText
+string_value = string_value + string_value
+
+string_value.lower()
+
+numbers = [1, 2, 3, 4, "Test", 10.5]  # list
+
+a = True  # bool
+a = False
+
+result = 10 > 5
--- basic/day_first/03-functions.py
+++ basic/day_first/03-functions.py
@@ -5,3 +5,31 @@
 #
 #  Работа с функциями, аргументы и возвращаемое значение
 #
+
+
+def my_print(message: str):
+    print("MY PRINT: " + message)
+
+
+my_print("some 1")
+my_print("some 2")
+
+
+def show_counter(step, max_step):
+    while step <= max_step:
+        print(step)
+        step += 1
+
+
+show_counter(5, 15)
+show_counter(10, 15)
+show_counter(10, 20)
+
+
+def sum_of_two_numbers(a, b):
+    return a + b
+
+
+answer = sum_of_two_numbers(20, 20)
+
+print(answer)
