a8ac7f53c2213ddafb3903ba8506d2ad5d3004fa
Artem Manchenkov <artyom@manchenkoff.me>
2019-08-16 21:37:29 +0300

    work in progress

--- src/server.py
+++ src/server.py
@@ -29,7 +29,10 @@ class Client(LineOnlyReceiver):
         - отправить сообщение приветствия
         """
 
-        pass
+        self.ip = self.transport.getPeer().host
+        self.factory.clients.append(self)
+
+        self.sendLine("Welcome".encode())
 
     def connectionLost(self, reason=connectionDone):
         """
@@ -39,7 +42,8 @@ class Client(LineOnlyReceiver):
         - вывести сообщение в чат об отключении
         """
 
-        pass
+        self.factory.clients.remove(self)
+        print(f"Client disconnected: {self.ip}")
 
     def lineReceived(self, line: bytes):
         """
@@ -49,7 +53,20 @@ class Client(LineOnlyReceiver):
         - переслать сообщение в чат, если уже зарегистрирован
         """
 
-        pass
+        message = line.decode()
+        print(message)
+
+        if self.login is None:
+            # login:admin
+            if message.startswith("login:"):
+                self.login = message.replace("login:", "")
+
+                notification = f"New client with login: {self.login}"
+
+                print(notification)
+                self.factory.notify_all_users(notification)
+        else:
+            self.factory.notify_all_users(message)
 
 
 class Server(ServerFactory):
@@ -66,12 +83,13 @@ class Server(ServerFactory):
         - вывод уведомления в консоль
         """
 
-        pass
+        self.clients = []
+        print("Server started - OK")
 
     def startFactory(self):
         """Запуск прослушивания клиентов (уведомление в консоль)"""
 
-        pass
+        print("Listening ...")
 
     def notify_all_users(self, message: str):
         """
@@ -79,7 +97,8 @@ class Server(ServerFactory):
         :param message: Текст сообщения
         """
 
-        pass
+        for user in self.clients:
+            user.sendLine(message.encode())
 
 
 if __name__ == '__main__':
