446065472baf5a7da2e7d7c5f593d3123fbbfbc4
Artem Manchenkov <artyom@manchenkoff.me>
2019-08-18 14:27:01 +0300

    homework updates

--- src/server.py
+++ src/server.py
@@ -34,9 +34,9 @@ class Client(LineOnlyReceiver):
         self.ip = self.transport.getPeer().host  # записываем IP адрес клиента
         self.factory.clients.append(self)  # добавляем в список клиентов фабрики
 
-        self.sendLine("Welcome to the chat!".encode())  # отправляем сообщение клиенту
+        self.sendLine("[Server] - Welcome to the chat!".encode())  # отправляем сообщение клиенту
 
-        print(f"Client {self.ip} connected")  # отображаем сообщение в консоли сервера
+        print(f"[Server] - Client {self.ip} connected")  # отображаем сообщение в консоли сервера
 
     def connectionLost(self, reason=connectionDone):
         """
@@ -48,7 +48,7 @@ class Client(LineOnlyReceiver):
 
         self.factory.clients.remove(self)  # удаляем клиента из списка в фабрике
 
-        print(f"Client {self.ip} disconnected")  # выводим уведомление в консоли сервера
+        print(f"[Server] - Client {self.ip} disconnected")  # выводим уведомление в консоли сервера
 
     def lineReceived(self, line: bytes):
         """
@@ -65,37 +65,45 @@ class Client(LineOnlyReceiver):
             if message.startswith("login:"):  # проверяем, чтобы в начале шел login:
                 user_login = message.replace("login:", "")  # вырезаем часть после :
 
-                # TODO: проверка существования логина
-                # for user in self.factory.clients:
-                #     if user_login == user.login:
-                #         error = f"Login {user_login} already exists!"
-                #         self.sendLine(error.encode())
-                #         self.transport.loseConnection()
-                #         return
+                # проверка существования логина
+                for user in self.factory.clients:
+                    if user_login == user.login:
+                        error = f"[Server] - Login {user_login} already exists!"
+
+                        self.sendLine(error.encode())
+                        """
+                        В интерфейсе можно не прерывать подключение, 
+                        а просто повторно написать "login:значение" в текстовом поле
+                        """
+                        # self.transport.loseConnection()
+                        return
 
                 self.login = user_login
 
-                notification = f"New user: {self.login}"  # формируем уведомление о новом клиенте
-                self.factory.notify_all_users(notification)  # отсылаем всем в чат
+                # отправка 10-ти сообщений новому клиенту
+                self.send_history()
 
-                # TODO: сделать отправку 10-ти сообщений новому клиенту
-                # self.send_history()
+                notification = f"[Server] - New user: {self.login}"  # формируем уведомление о новом клиенте
+                self.factory.notify_all_users(notification)  # отсылаем всем в чат
             else:
-                self.sendLine("Invalid login".encode())  # шлем уведомление, если в сообщении ошибка
+                self.sendLine("[Server] - Invalid login".encode())  # шлем уведомление, если в сообщении ошибка
         # если логин уже есть и это следующее сообщение
         else:
             format_message = f"{self.login}: {message}"  # форматируем сообщение от имени клиента
 
-            # TODO: сохранять сообщения в список
-            # self.factory.messages.append(format_message)
+            # сохранять сообщения в список
+            self.factory.messages.append(format_message)
 
             # отсылаем всем в чат и в консоль сервера
             self.factory.notify_all_users(format_message)
             print(format_message)
 
-    # def send_history(self):
-    #     # отправка последних 10 сообщений
-    #     pass
+    def send_history(self):
+        """Отправка новому клиенту последних 10-ти сообщений"""
+        last_messages = self.factory.messages[-10:]
+
+        for message in last_messages:
+            self.sendLine(message.encode())
 
 
 class Server(ServerFactory):
@@ -116,12 +124,12 @@ class Server(ServerFactory):
         self.clients = []  # создаем пустой список клиентов
         self.messages = []
 
-        print("Server started - OK")  # уведомление в консоль сервера
+        print("[Server] - Server started - OK")  # уведомление в консоль сервера
 
     def startFactory(self):
         """Запуск прослушивания клиентов (уведомление в консоль)"""
 
-        print("Start listening ...")  # уведомление в консоль сервера
+        print("[Server] - Start listening ...")  # уведомление в консоль сервера
 
     def notify_all_users(self, message: str):
         """
